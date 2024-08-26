d90c74f8d2fdf65720b803adb3823994636924b4
halehdizaji <h13.dizaji@gmail.com>
2024-08-26 21:39:25 +0330

    Delete data preparation/text processing directory

--- data preparation/text processing/Str_Processing.py
+++
@@ -1,8 +0,0 @@
-import ast
-
-def cnvrt_str_list_to_int_list(str_list):
-    res = ast.literal_eval(ini_list)
-
-    # printing final result and its type
-    print("final list", res)
-    print(type(res))
